19d70d0c0edc2173b68e9c3ef70e7879ca89dd8e
Salman Alfast <salmanalfast@gmail.com>
2023-03-24 11:26:47 +0800

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,3 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false, home: DisclaimerClass());
-  }
-}
